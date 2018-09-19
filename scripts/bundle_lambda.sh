0da3b72341ea25bae389f9b757650c76d2cd8a39
Steve Norum <stevenorum@gmail.com>
2018-09-19 09:34:27 -0400

    Fixing bundle script

--- scripts/bundle_lambda.sh
+++ scripts/bundle_lambda.sh
@@ -3,5 +3,4 @@
 # Once the code and all that is in the build/ directory,
 # process the template and get it ready for deployment.
 
-python3 generate_lambda_template.py SamTemplate.json ProcessedSamTemplate.json
-python3 process_static_functions.py ProcessedSamTemplate.json ProcessedSamTemplate.json
+aws cloudformation package --template-file SamTemplate.json --s3-bucket $S3_BUCKET --output-template-file NewSamTemplate.json --use-json
