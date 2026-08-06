e14285eed2d298dc639ceef1573da22aed497f2c
stevenorum <stevenorum@gmail.com>
2026-08-06 15:39:07 -0400

    Switching the default stack for deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ if [ -f private.sh ]; then
     source private.sh
 fi
 
-scripts/build.sh && cdk synth && cdk deploy --profile $PROFILE_NAME DeploymentPipelineStack
+scripts/build.sh && cdk synth && cdk deploy --profile $PROFILE_NAME HelloworldCdkPythonStack
