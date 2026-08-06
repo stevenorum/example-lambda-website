d366504a48d8854f705faadb7673a22404d99c62
stevenorum <stevenorum@gmail.com>
2026-08-06 15:20:58 -0400

    More changes.

--- stack_code/app.py
+++ stack_code/app.py
@@ -3,8 +3,10 @@ import os
 
 import aws_cdk
 
-from stack_code.python_lambda_website import PythonLambdaWebsiteStack
-from stack_code.deployment_pipeline import DeploymentPipelineStack
+# from stack_code.python_lambda_website import PythonLambdaWebsiteStack
+# from stack_code.deployment_pipeline import DeploymentPipelineStack
+from python_lambda_website import PythonLambdaWebsiteStack
+from deployment_pipeline import DeploymentPipelineStack
 
 OWNER = "stevenorum"
 REPO = "example-lambda-website"
@@ -14,7 +16,13 @@ ENV = aws_cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.ge
 
 app = aws_cdk.App()
 
-DeploymentPipelineStack(app, "DeploymentPipelineStack", env=ENV, owner=OWNER, repo=REPO, branch=BRANCH)
+auth_token = aws_cdk.SecretValue.secrets_manager(
+    # obviously this'll be different for your account
+    secret_id="arn:aws:secretsmanager:us-east-1:959113775746:secret:prod/github-KzCYji",
+    json_field="codesuite_oauth"
+)
+
+DeploymentPipelineStack(app, "DeploymentPipelineStack", env=ENV, owner=OWNER, repo=REPO, branch=BRANCH, auth_token=auth_token)
 
 PythonLambdaWebsiteStack(app, "HelloworldCdkPythonStack", env=ENV)
 
--- stack_code/deployment_pipeline.py
+++ stack_code/deployment_pipeline.py
@@ -6,11 +6,13 @@ from aws_cdk.pipelines import CodePipeline, CodePipelineSource, ShellStep
 
 class DeploymentPipelineStack(aws_cdk.Stack):
 
+    # the docs recommend using CodeStar instead of CodePipelineSource.git_hub, but this works for now and is how their example tutorial does it
     def __init__(self, scope: Construct, construct_id: str,
                  owner: str,
                  repo: str,
                  branch: str = "main",
                  pipeline_name: Optional[str] = None,
+                 auth_token: Optional[aws_cdk.SecretValue] = None,
                  **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
         # May need to add some sanitization to ensure that this pipeline name meets the CodePipeline requirements.
@@ -20,7 +22,11 @@ class DeploymentPipelineStack(aws_cdk.Stack):
             pipeline_name=pipeline_name,
             synth=ShellStep(
                 "Synth",
-                input=CodePipelineSource.git_hub(f"{owner}/{repo}", branch),
+                input=CodePipelineSource.git_hub(
+                    repo_string=f"{owner}/{repo}",
+                    branch=branch,
+                    authentication=auth_token,
+                ),
                 commands=[
                     "npm install -g aws-cdk",
                     "python -m pip install -r requirements.txt",
